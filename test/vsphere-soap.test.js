1637aaafc166f3b352c14d1fb604f3579be718c4
reedog117 <patrickc81@gmail.com>
2015-02-10 11:27:58 -0500

    created separate internal _commandExecutor class; sessions now persistent; multiple commands can run simultaneously

--- test/vsphere-soap.test.js
+++ test/vsphere-soap.test.js
@@ -6,8 +6,10 @@
 
 var Code = require('code');
 var Lab = require('lab');
+var util = require('util');
 var lab = exports.lab = Lab.script();
-var vCenterConnectionInstance = require('../lib/vcenterconnectioninstance.js').vCenterConnectionInstance;
+var vc = require('../lib/client');
+var TestCreds = require('../config.js').vCenterTestCreds
 
 var describe = lab.describe;
 var it = lab.it;
@@ -15,23 +17,71 @@ var before = lab.before;
 var after = lab.after;
 var expect = Code.expect;
 
+var VItest = new vc.Client(TestCreds.vCenterIP, TestCreds.vCenterUser, TestCreds.vCenterPassword, false);
 
-describe('vCenterConnectionInstance object initialization:', function(){
-
-  var VItest = new vCenterConnectionInstance('192.168.103.160','root','h@x0r!n!t', false);
+describe('vCenterClient object initialization:', function(){
 
   it('provides a successful login', {timeout: 20000}, function(done) {
     
-    VItest.on('authenticated', function() {
+    VItest.on('ready', function() {
       expect(VItest.userName).to.exist();
       //console.log('logged in user : ' + VItest.userName);
       expect(VItest.fullName).to.exist();
       //console.log('logged in user fullname : ' + VItest.fullName);
+      expect(VItest.serviceContent).to.exist();
+      //console.log(VItest.serviceContent);
       done();
     });
 
     // if login works, can run the subsequent tests
-
   });
+
+});
+
+describe('v')
+
+// these tests don't work yet
+describe('vCenterConnectionInstance query commands:', function(){
+
+  // make sure we have a successfully authenticated instance
+  // before testing
+
+  /*before( {timeout: 5000}, function(done){
+    VItest.on('authenticated', function() {
+      // get property collector
+      propertyCollector = VItest.serviceContent.propertyCollector;
+      // get view manager
+      viewManager = VItest.serviceContent.viewManager;
+      done();
+    });
+  });
+  */
+
+  it('retrieves current time', {timeout: 20000}, function(done){
+    // get property collector
+    //var propertyCollector = VItest.serviceContent.propertyCollector;
+    // get view manager
+    //var viewManager = VItest.serviceContent.viewManager;
+    VItest.runCommand('CurrentTime', { _this: 'ServiceInstance'} )
+      .on('result', function(result){
+        //console.log('result retrieved : ' + util.inspect(result.returnval));
+        expect(result.returnval).to.be.a.date();
+        done();
+    });
+  }); 
+
+
+  it('retrieves current time 2 (check for event clobbering)', {timeout: 20000}, function(done){
+    // get property collector
+    //var propertyCollector = VItest.serviceContent.propertyCollector;
+    // get view manager
+    //var viewManager = VItest.serviceContent.viewManager;
+    VItest.runCommand('CurrentTime', { _this: 'ServiceInstance'} )
+      .on('result', function(result){
+        //console.log('result retrieved : ' + util.inspect(result.returnval));
+        expect(result.returnval).to.be.a.date();
+        done();
+    });
+  }); 
 
 });
