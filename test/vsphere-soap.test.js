eac2f76209ca1cad0ebea6424507d2c8a343ba33
reedog117 <patrickc81@gmail.com>
2015-02-11 00:03:48 -0500

    test to retrieve all vms working

--- test/vsphere-soap.test.js
+++ test/vsphere-soap.test.js
@@ -10,6 +10,8 @@ var util = require('util');
 var lab = exports.lab = Lab.script();
 var vc = require('../lib/client');
 var TestCreds = require('../config.js').vCenterTestCreds
+var _ = require('lodash');
+
 
 var describe = lab.describe;
 var it = lab.it;
@@ -20,7 +22,6 @@ var expect = Code.expect;
 var VItest = new vc.Client(TestCreds.vCenterIP, TestCreds.vCenterUser, TestCreds.vCenterPassword, false);
 
 describe('Client object initialization:', function(){
-
   it('provides a successful login', {timeout: 5000}, function(done) {
     
     VItest.once('ready', function() {
@@ -31,11 +32,9 @@ describe('Client object initialization:', function(){
       expect(VItest.serviceContent).to.exist();
       //console.log(VItest.serviceContent);
       done();
+
     });
-
-    // if login works, can run the subsequent tests
   });
-
 });
 
 describe('Client reconnection test:', function(){
@@ -44,8 +43,7 @@ describe('Client reconnection test:', function(){
 
     VItest.runCommand('Logout', { _this: VItest.serviceContent.sessionManager })
       .once('result', function(result){
-        //console.log('logout result : ' + util.inspect(result, {depth: null} ));
-        // we're logged out, so now run another command to force logging in again
+        // now we're logged out, so let's try running a command to test automatic re-login
         VItest.runCommand('CurrentTime', { _this: 'ServiceInstance'} )
           .once('result', function(result) {
             expect(result.returnval).to.be.a.date();
@@ -59,41 +57,87 @@ describe('Client reconnection test:', function(){
 describe('Client tests - query commands:', function(){
 
   it('retrieves current time', {timeout: 5000}, function(done){
-    // get property collector
-    //var propertyCollector = VItest.serviceContent.propertyCollector;
-    // get view manager
-    //var viewManager = VItest.serviceContent.viewManager;
+
     VItest.runCommand('CurrentTime', { _this: 'ServiceInstance'} )
       .once('result', function(result){
-        //console.log('result retrieved : ' + util.inspect(result.returnval));
         expect(result.returnval).to.be.a.date();
         done();
     });
   }); 
-
 
   it('retrieves current time 2 (check for event clobbering)', {timeout: 5000}, function(done){
-    // get property collector
-    //var propertyCollector = VItest.serviceContent.propertyCollector;
-    // get view manager
-    //var viewManager = VItest.serviceContent.viewManager;
+
     VItest.runCommand('CurrentTime', { _this: 'ServiceInstance'} )
       .once('result', function(result){
-        //console.log('result retrieved : ' + util.inspect(result.returnval));
         expect(result.returnval).to.be.a.date();
         done();
     });
   }); 
 
-  it('can obtain the VM inventory', {timeout: 20000}, function(done){
+  it('can obtain the names of all Virtual Machines in the inventory', {timeout: 20000}, function(done){
+    // get property collector
+    var propertyCollector = VItest.serviceContent.propertyCollector;
+    // get view manager
+    var viewManager = VItest.serviceContent.viewManager;
+    // get root folder
+    var rootFolder = VItest.serviceContent.rootFolder;
 
+    var containerView,
+      objectSpec,
+      traversalSpec,
+      propertySpec,
+      propertyFilterSpec;
     // this is the equivalent to 
+    VItest.runCommand('CreateContainerView', { _this: viewManager, container: rootFolder, type: ['VirtualMachine'], recursive: true})
+    .once('result', function(result){
 
+      // build all the data structures needed to query all the vm names
+      containerView = result.returnval;
 
+      objectSpec = {
+        attributes: { 'xsi:type': 'ObjectSpec'}, // setting attributes xsi:type is important or else the server may mis-recognize types!
+        obj: containerView,
+        skip: true
+      };
 
+      traversalSpec = {
+        attributes: { 'xsi:type': 'TraversalSpec'}, 
+        name: 'traverseEntities', 
+        type: 'ContainerView', 
+        path: 'view', 
+        skip: false, 
+      };
+      
+      objectSpec = _.assign(
+        objectSpec,
+        { selectSet: [ traversalSpec ] }
+      );
+      
+      propertySpec = {
+        attributes: { 'xsi:type': 'PropertySpec'},
+        type: 'VirtualMachine',
+        pathSet: ['name']
+      };
 
+      propertyFilterSpec = {
+        attributes: { 'xsi:type': 'PropertyFilterSpec'},
+        propSet: [ propertySpec ],
+        objectSet: [ objectSpec ]
+      };
+      // TODO: research why it fails if propSet is declared after objectSet
 
+      VItest.runCommand('RetrievePropertiesEx', { _this: propertyCollector, specSet: [ propertyFilterSpec ], options: {attributes: {type:'RetrieveOptions'}} })
+      .once('result', function(result, raw){
 
-    done();
+        expect(result.returnval.objects).to.exist();
+        expect(result.returnval.objects.obj.attributes.type).to.be.equal('VirtualMachine');
+        done();
+      })
+      .once('error', function(err){
+        console.log('\n\nlast request : ' + VItest.client.lastRequest);
+      });
+
+    });
+
   });
 });
