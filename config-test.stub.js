5e34ce1002a056cbe88fa0781f3fb9815cfa559c
reedog117 <patrickc81@gmail.com>
2015-02-11 00:58:43 -0500

    updated docs, testing stub

--- config-test.stub.js
+++ config-test.stub.js
@@ -1,7 +1,7 @@
 // place your own credentials here for a vCenter or ESXi server
 // this information will be used for connecting to a vCenter instance
 // for module testing
-
+// name the file config-test.js
 
 var vCenterTestCreds = {
 	'vCenterIP' : 'vcsa',
