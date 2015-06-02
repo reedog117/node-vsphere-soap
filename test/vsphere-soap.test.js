338eed6eb89fa8b7a51ca80c57d7ae9d03df51c7
reedog117 <patrickc81@gmail.com>
2015-06-02 13:03:16 -0400

    code cleanup; temporarily locked soap to 0.8.0

--- test/vsphere-soap.test.js
+++ test/vsphere-soap.test.js
@@ -21,10 +21,10 @@ var expect = Code.expect;
 
 var VItest = new vc.Client(TestCreds.vCenterIP, TestCreds.vCenterUser, TestCreds.vCenterPassword, false);
 
-describe('Client object initialization:', function(){
-  it('provides a successful login', {timeout: 5000}, function(done) {
+describe('Client object initialization:', function () {
+  it('provides a successful login', {timeout: 5000}, function (done) {
     
-    VItest.once('ready', function() {
+    VItest.once('ready', function () {
       expect(VItest.userName).to.exist();
       //console.log('logged in user : ' + VItest.userName);
       expect(VItest.fullName).to.exist();
@@ -33,54 +33,66 @@ describe('Client object initialization:', function(){
       //console.log(VItest.serviceContent);
       done();
 
+    })
+    .once('error', function (err) {
+      
+      console.error(err);
+      // this should fail if there's a problem
+      expect(VItest.userName).to.exist();
+      //console.log('logged in user : ' + VItest.userName);
+      expect(VItest.fullName).to.exist();
+      //console.log('logged in user fullname : ' + VItest.fullName);
+      expect(VItest.serviceContent).to.exist();
+      //console.log(VItest.serviceContent);
+      done();
     });
   });
 });
 
-describe('Client reconnection test:', function(){
+describe('Client reconnection test:', function () {
 
-  it('can successfully reconnect', {timeout: 5000}, function(done) {
+  it('can successfully reconnect', {timeout: 5000}, function (done) {
 
     VItest.runCommand('Logout', { _this: VItest.serviceContent.sessionManager })
-      .once('result', function(result){
+      .once('result', function (result){
         // now we're logged out, so let's try running a command to test automatic re-login
         VItest.runCommand('CurrentTime', { _this: 'ServiceInstance'} )
-          .once('result', function(result) {
+          .once('result', function (result) {
             expect(result.returnval).to.be.a.date();
             done();
           })
-          .once('error', function(err) {
+          .once('error', function (err) {
             console.error(err);
           });
       })
-      .once('error', function(err) {
+      .once('error', function (err) {
         console.error(err);
       });
   });
 });
 
 // these tests don't work yet
-describe('Client tests - query commands:', function(){
+describe('Client tests - query commands:', function (){
 
-  it('retrieves current time', {timeout: 5000}, function(done){
+  it('retrieves current time', {timeout: 5000}, function (done){
 
     VItest.runCommand('CurrentTime', { _this: 'ServiceInstance'} )
-      .once('result', function(result){
+      .once('result', function (result){
         expect(result.returnval).to.be.a.date();
         done();
     });
   }); 
 
-  it('retrieves current time 2 (check for event clobbering)', {timeout: 5000}, function(done){
+  it('retrieves current time 2 (check for event clobbering)', {timeout: 5000}, function (done){
 
     VItest.runCommand('CurrentTime', { _this: 'ServiceInstance'} )
-      .once('result', function(result){
+      .once('result', function (result){
         expect(result.returnval).to.be.a.date();
         done();
     });
   }); 
 
-  it('can obtain the names of all Virtual Machines in the inventory', {timeout: 20000}, function(done){
+  it('can obtain the names of all Virtual Machines in the inventory', {timeout: 20000}, function (done){
     // get property collector
     var propertyCollector = VItest.serviceContent.propertyCollector;
     // get view manager
@@ -95,7 +107,7 @@ describe('Client tests - query commands:', function(){
       propertyFilterSpec;
     // this is the equivalent to 
     VItest.runCommand('CreateContainerView', { _this: viewManager, container: rootFolder, type: ['VirtualMachine'], recursive: true})
-    .once('result', function(result){
+    .once('result', function (result){
 
       // build all the data structures needed to query all the vm names
       containerView = result.returnval;
@@ -133,7 +145,7 @@ describe('Client tests - query commands:', function(){
       // TODO: research why it fails if propSet is declared after objectSet
 
       VItest.runCommand('RetrievePropertiesEx', { _this: propertyCollector, specSet: [ propertyFilterSpec ], options: {attributes: {type:'RetrieveOptions'}} })
-      .once('result', function(result, raw){
+      .once('result', function (result, raw){
 
         expect(result.returnval.objects).to.exist();
         if( _.isArray(result.returnval.objects) ) {
@@ -143,7 +155,7 @@ describe('Client tests - query commands:', function(){
         }
         done();
       })
-      .once('error', function(err){
+      .once('error', function (err){
         console.log('\n\nlast request : ' + VItest.client.lastRequest);
       });
 
